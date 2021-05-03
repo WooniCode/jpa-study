5897d1f715622b26dbccc5d8234aa787ae9e4e08
woonijung <jdu2038@gmail.com>
2021-05-03 20:41:22 +0900

    시퀀스 제너레이터

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,12 +16,27 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setId(10L);
-            member.setUsername("A");
-            member.setRoleType(RoleType.ADMIN);
+            Member member1 = new Member();
+            member1.setUsername("A");
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("===================");
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("Member1.memberId: " + member1.getId());
+            System.out.println("Member2.memberId: " + member2.getId());
+            System.out.println("Member3.memberId: " + member3.getId());
+
+            System.out.println("===================");
+
             tx.commit();
         } catch(Exception e) {
             tx.rollback();
