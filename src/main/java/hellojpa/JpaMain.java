073247fa85dc72451651fba4195b96a27dfd4f21
woonijung <jdu2038@gmail.com>
2021-05-08 14:55:42 +0900

    단방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,26 +15,19 @@ public class JpaMain {
 
         try {
 
-            Member member1 = new Member();
-            member1.setUsername("A");
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-            Member member2 = new Member();
-            member2.setUsername("B");
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
 
-            Member member3 = new Member();
-            member3.setUsername("C");
+            Member findMember = em.find(Member.class, member.getId());
 
-            System.out.println("===================");
-
-            em.persist(member1);
-            em.persist(member2);
-            em.persist(member3);
-
-            System.out.println("Member1.memberId: " + member1.getId());
-            System.out.println("Member2.memberId: " + member2.getId());
-            System.out.println("Member3.memberId: " + member3.getId());
-
-            System.out.println("===================");
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam: " + findTeam.getName());
 
             tx.commit();
         } catch(Exception e) {
